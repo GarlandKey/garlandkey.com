656b53e88a31d530af257b23df9695e1f89daf79
Garland Key <garlandkey@protonmail.com>
2022-04-09 18:08:48 -0500

    setup sections and started js

---
+++ js/main.js
@@ -0,0 +1,4 @@
+const home = document.querySelector('#home').classList.add('hidden')
+const services = document.querySelector('#services').classList.add('hidden')
+const portfolio = document.querySelector('#portfolio').classList.add('hidden')
+const contact = document.querySelector('#contact').classList.add('hidden')
