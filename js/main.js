d37fc8a0574fcd848a0fef302f1964b616f7796a
Garland Key <garlandkey@protonmail.com>
2022-04-10 03:07:36 -0500

    optimized all images, completed home and portfolio pages

--- js/main.js
+++ js/main.js
@@ -1,4 +1,44 @@
-const home = document.querySelector('#home').classList.add('hidden')
-const services = document.querySelector('#services').classList.add('hidden')
-const portfolio = document.querySelector('#portfolio').classList.add('hidden')
-const contact = document.querySelector('#contact').classList.add('hidden')
+/* TODO -- refector so it doesn't look like a baddie did this. */
+
+const home = document.querySelector('.event-home')
+const services = document.querySelector('.event-services')
+const portfolio = document.querySelector('.event-portfolio')
+const contact = document.querySelector('.event-contact')
+
+const sectionHome = document.querySelector('#home')
+const sectionServices = document.querySelector('#services')
+const sectionPortfolio = document.querySelector('#portfolio')
+const sectionContact = document.querySelector('#contact')
+
+home.addEventListener('click', showHome)
+services.addEventListener('click', showServices)
+portfolio.addEventListener('click', showPortfolio)
+contact.addEventListener('click', showContact)
+
+function showHome() {
+    sectionHome.classList.remove('hidden')
+    sectionServices.classList.add('hidden')
+    sectionPortfolio.classList.add('hidden')
+    sectionContact.classList.add('hidden')
+}
+
+function showServices() {
+    sectionHome.classList.add('hidden')
+    sectionServices.classList.remove('hidden')
+    sectionPortfolio.classList.add('hidden')
+    sectionContact.classList.add('hidden')
+}
+
+function showPortfolio() {
+    sectionHome.classList.add('hidden')
+    sectionServices.classList.add('hidden')
+    sectionPortfolio.classList.remove('hidden')
+    sectionContact.classList.add('hidden')
+}
+
+function showContact() {
+    sectionHome.classList.add('hidden')
+    sectionServices.classList.add('hidden')
+    sectionPortfolio.classList.add('hidden') 
+    sectionContact.classList.remove('hidden')
+}
