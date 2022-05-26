71ece47bfea3d226f2b7570a97d1d68156b6643e
Garland Key <garlandkey@protonmail.com>
2022-05-26 17:05:21 -0500

    created a js object for the skills section

--- v2/js/main.js
+++ v2/js/main.js
@@ -1,7 +1,21 @@
-const colorCSS = document.querySelector('skills').backgroundColor = '#264de4'
-const colorHTML = document.querySelector('skills').backgroundColor = '#e44d26'
-const colorJS = document.querySelector('skills').backgroundColor = '#f4dd40'
-const colorMongoDB = document.querySelector('skills').backgroundColor = '#00684a'
-const colorNodeJS = document.querySelector('skills').backgroundColor = '#333333'
-const colorPostgreSQL = document.querySelector('skills').backgroundColor = '#336791'
-const colorReact = document.querySelector('skills').backgroundColor = '#282c34'
+let skills = {
+    bkgColor : 'document.querySelector('skills').backgroundColor',
+    blurb: {
+        css: 'I wield CSS3 to make beautiful, responsive web applications, always considering the separation of concerns.',
+        html: 'I use semantic HTML to layout elements in an easy-to-understand way.',
+        js: 'I use an object-oriented approach to JavaScript, paying particular attention to style guides for maximum organization and reusability.',
+        mongodb: 'I work with MongoDB, allowing me to pull results in a JSON format quickly.',
+        nodejs: 'I utilize the asynchronous capabilities of Node.js to avoid blocking and ensure my web applications are fast.',
+        postgresql: 'When dealing with compatibility and large numbers of tables, I use PostgreSQL for its reliability and ability to scale up.',
+        react: 'I use React.js to build user interfaces for its flexibility across browsers and operating systems.'
+    },
+    colors: {
+        css: '#264de4',
+        html: '#e44d26',
+        js: '#f4dd40',
+        mongodb: '#00684a',
+        nodejs: '#333333',
+        postgresql: '#336791',
+        react: '#282c34'
+    }
+}
