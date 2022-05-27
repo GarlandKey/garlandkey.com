08637a9e03abdb4c912c54f437959984378e3c05
Garland Key <garlandkey@protonmail.com>
2022-05-26 19:15:45 -0500

    added new images

--- v2/js/main.js
+++ v2/js/main.js
@@ -1,20 +1,26 @@
 let skills = {
-    bkgColor : 'document.querySelector('skills').backgroundColor',
+    bkgColor : 'document.querySelector("skills").backgroundColor',
     blurb: {
         css: 'I wield CSS3 to make beautiful, responsive web applications, always considering the separation of concerns.',
         html: 'I use semantic HTML to layout elements in an easy-to-understand way.',
+        illustrator: 'I use Adobe Illustrator to create ',
         js: 'I use an object-oriented approach to JavaScript, paying particular attention to style guides for maximum organization and reusability.',
+        lightroom: '',
         mongodb: 'I work with MongoDB, allowing me to pull results in a JSON format quickly.',
         nodejs: 'I utilize the asynchronous capabilities of Node.js to avoid blocking and ensure my web applications are fast.',
+        photoshop: '',
         postgresql: 'When dealing with compatibility and large numbers of tables, I use PostgreSQL for its reliability and ability to scale up.',
         react: 'I use React.js to build user interfaces for its flexibility across browsers and operating systems.'
     },
     colors: {
         css: '#264de4',
         html: '#e44d26',
+        illustrator: '#330000',
         js: '#f4dd40',
+        lightroom: '#001e36',
         mongodb: '#00684a',
         nodejs: '#333333',
+        photoshop: '#001e36',
         postgresql: '#336791',
         react: '#282c34'
     }
