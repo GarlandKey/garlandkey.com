aff5fe0488ebaf9b06fb1c638627b6fd9e4cfeda
Garland Key <garlandkey@protonmail.com>
2022-05-28 18:42:14 -0500

    working on initial js

--- v2/js/main.js
+++ v2/js/main.js
@@ -1,27 +1,44 @@
-let skills = {
-    bkgColor : 'document.querySelector("skills").backgroundColor',
-    blurb: {
-        css: 'I wield CSS3 to make beautiful, responsive web applications, always considering the separation of concerns.',
-        html: 'I use semantic HTML to layout elements in an easy-to-understand way.',
-        illustrator: 'I use Adobe Illustrator to create ',
-        js: 'I use an object-oriented approach to JavaScript, paying particular attention to style guides for maximum organization and reusability.',
-        lightroom: '',
-        mongodb: 'I work with MongoDB, allowing me to pull results in a JSON format quickly.',
-        nodejs: 'I utilize the asynchronous capabilities of Node.js to avoid blocking and ensure my web applications are fast.',
-        photoshop: '',
-        postgresql: 'When dealing with compatibility and large numbers of tables, I use PostgreSQL for its reliability and ability to scale up.',
-        react: 'I use React.js to build user interfaces for its flexibility across browsers and operating systems.'
-    },
-    colors: {
-        css: '#264de4',
-        html: '#e44d26',
-        illustrator: '#330000',
-        js: '#f4dd40',
-        lightroom: '#001e36',
-        mongodb: '#00684a',
-        nodejs: '#333333',
-        photoshop: '#001e36',
-        postgresql: '#336791',
-        react: '#282c34'
-    }
+let colors = {
+    css: '#264de4',
+    html: '#e44d26',
+    illustrator: '#330000',
+    js: '#f4dd40',
+    lightroom: '#001e36',
+    mongodb: '#00684a',
+    nodejs: '#333333',
+    photoshop: '#001e36',
+    postgresql: '#336791',
+    react: '#282c34'
 }
+
+let selectors = {
+    blurb: document.querySelector('.blurb'),
+    skills: document.querySelector('.skills'),
+    imgCSS: document.querySelector('#imgCSS'),
+    imgHTML: document.querySelector('#imgHTML'),
+    imgAI: document.querySelector('#imgAI'),
+    imgJS: document.querySelector('#imgJS'),
+    imgLR: document.querySelector('#imgLR'),
+    imgMongo: document.querySelector('#imgMongo'),
+    imgNode: document.querySelector('#imgNode'),
+    imgPS: document.querySelector('#imgPS'),
+    imgSQL: document.querySelector('#imgSQL'),
+    imgReact: document.querySelector('#imgReact'),
+    pCSS: document.querySelector('#pCSS'),
+    pHTML: document.querySelector('#pHTML'),
+    pJS: document.querySelector('#pJS'),
+    pMongo: document.querySelector('#pMongo'),
+    pNode: document.querySelector('#pNode'),
+    pSQL: document.querySelector('#pSQL'),
+    pReact: document.querySelector('#pReact')
+}
+
+function css() {
+    console.log('hover detected')
+    console.log(colors.css)
+    selectors.skills.style.backgroundColor = colors.css
+    selectors.blurb.classList.toggle('hidden')
+    selectors.pCSS.classList.toggle('hidden')
+}
+
+selectors.imgCSS.addEventListener('mouseover', css);
